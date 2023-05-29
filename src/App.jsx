5ec31a129b08e0b62264910477cd41532d5dc02c
Micaela Ceballos <micaelaceballos411@gmail.com>
2023-05-29 15:09:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Users from "./components/Users";
+import Header from "./components/Header";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getUsers } from "./redux/userSlice";
+
+function App() {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.users.users);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => dispatch(getUsers(data)))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className="flex justify-center items-center flex-col text-center">
+      <Header />
+      <Users data={data} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div className="bg-red-200">Placeholder</div>;
+};
+
+export default Header;
